4ee316cdc7e6d1994f27fe1fa898b58903d8c113
Taher Soliman <taherelbadawy38@gmail.com>
2022-01-31 10:05:15 +0200

    Adjust Side-bar

--- Assets/Js/Main/Main.js
+++ Assets/Js/Main/Main.js
@@ -1,9 +1,41 @@
+// Side bar Navigation
+const sideBarBtn = document.querySelector(".toggle-nav-btn");
+const sideBar = document.querySelector("#sidebar");
+const pageContent = document.querySelector(".page-content");
+const logoImg = document.querySelector(".logo");
+const windowWidth = window.innerWidth;
+sideBarBtn.addEventListener("click", () => {
+  sideBar.classList.toggle("toggle-activate");
+  pageContent.classList.toggle("activated-aside");
+  if (sideBar.classList.contains("toggle-activate")) {
+    logoImg.classList.add("d-none");
+  } else if (window.innerWidth > 991) {
+    logoImg.classList.add("d-none");
+  } else {
+    logoImg.classList.remove("d-none");
+  }
+});
+//Try Media
+
+setInterval(() => {
+  console.log(window.innerWidth);
+  console.log(window.innerWidth > 991);
+}, 2000);
+// Trigger Sticky Nav Bar
 window.addEventListener("scroll", function () {
   let nav = document.querySelector("#nav-bar");
   nav.classList.toggle("sticky-nav", window.scrollY > 0);
 });
-// Trigger Owl Carusel
-$(".owl-carousel").owlCarousel({
+// Check opend from mobile
+function checkMopile() {
+  if (window.innerWidth < 991) {
+    sideBar.classList.add("toggle-activate");
+    console.log("mobile");
+  } else console.log("not mobile");
+}
+checkMopile();
+// Trigger Owl Carusel For Banner
+/*$(".owl-carousel.owl-banner").owlCarousel({
   loop: true,
   margin: 10,
   nav: true,
@@ -18,4 +50,4 @@ $(".owl-carousel").owlCarousel({
       items: 1,
     },
   },
-});
+});*/
